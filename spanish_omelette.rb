7ad6defa5278f3365b9655d14efa30ffae1f0d05
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-06-28 15:26:22 +0200

    Iterate through each of steps

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -123,4 +123,17 @@ def ask_if_ready(step, index)
   answer.upcase == 'Y'
 end
 
-ask_if_ready
+#Iterating through Each of the Steps
+steps.each_with_index do |step, index|
+  print_divider
+
+  loop do
+    ready = ask_if_ready(step, index)
+    break if ready
+
+    puts "OK, I will give you some extra time."
+    print_progress_bar
+  end
+
+  send(step[:action])
+end
